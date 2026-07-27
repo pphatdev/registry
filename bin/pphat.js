974be64d9f36e38b0fffcec34d4853318b3f2622
pphatdev <pphat.kits@gmail.com>
2026-07-27 21:43:21 +0700

    feat: implement CLI executable shims and automate npm publishing via CI/CD workflow

--- bin/pphat.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-process.env.PPHAT_INVOKED_AS = 'pphat';
-require('../dist/index.js');
--- bin/pphatdev.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-process.env.PPHAT_INVOKED_AS = 'pphatdev';
-require('../dist/index.js');
